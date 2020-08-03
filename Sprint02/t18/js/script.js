3469d3a05b542e5d2bee03b4f40fcc4c7d0f73c1
Dumagenon <simonov.pasha9822@gmail.com>
2020-08-03 17:24:00 +0300

    Task t18 - Done

---
+++ Sprint02/t18/js/script.js
@@ -0,0 +1,10 @@
+function sortEvenOdd(arr) {
+  return arr.sort(function(a, b) {
+    return a % 2 - b % 2 || a - b;
+  });
+}
+
+const arr= [6, 2, 15, 5, 1, 3, 8, 1, 8, 10, 7, 11];
+
+sortEvenOdd(arr);
+console.log(arr); // (12) [2, 6, 8, 8, 10, 1, 1, 3, 5, 7, 11, 15]
