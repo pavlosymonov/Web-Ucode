1ada1818c6b618559fa86f22846005e8d1f93594
Paul <simonov.pasha9822@gmail.com>
2020-10-19 18:34:48 +0300

    Updates

---
+++ precache-manifest.5cfee0595611e87a1113d36599fc2a49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba33e716b7dbaacc64cece29b0ab97d",
+    "url": "/Web-Ucode/tree/master/usof/index.html"
+  },
+  {
+    "revision": "b6cd67d99795d5e198df",
+    "url": "/Web-Ucode/tree/master/usof/static/css/main.05640100.chunk.css"
+  },
+  {
+    "revision": "182b7434d26970e9640b",
+    "url": "/Web-Ucode/tree/master/usof/static/js/2.6a8ea22d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Web-Ucode/tree/master/usof/static/js/2.6a8ea22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cd67d99795d5e198df",
+    "url": "/Web-Ucode/tree/master/usof/static/js/main.f5ffc43d.chunk.js"
+  },
+  {
+    "revision": "09f066e82e8a1953e604",
+    "url": "/Web-Ucode/tree/master/usof/static/js/runtime-main.f1137537.js"
+  },
+  {
+    "revision": "a613aa267c7f68fa7ec2326c6133629a",
+    "url": "/Web-Ucode/tree/master/usof/static/media/logo-stackoverflow.a613aa26.svg"
+  }
+]);
